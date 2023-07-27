35fa9fdb243e276152e92fed1635a2ba37b50ca2
Bryan Stapleton <stapleton_bryan@yahoo.com>
2023-07-27 09:18:55 -0600

    shader exp

--- src/utils/materials/wave.ts
+++ src/utils/materials/wave.ts
@@ -24,21 +24,21 @@ const WaveMaterial = shaderMaterial(
       varying vec2 vUv;      
 
       vec3 palette(float t) {
-        vec3 a = vec3(0.25, 0.25, 0.25);
+        vec3 a = vec3(0.5, 0.5, 0.5);
         vec3 b = vec3(0.5, 0.5, 0.5);
-        vec3 c = vec3(1.0, 1.0, 1.0);
-        vec3 d = vec3(0.263 / time, 0.416 / time, 0.557 / time);
-        return a - b * cos(6.28318 * (c * t - d));
+        vec3 c = vec3(1.50, 1.5, 1.5);
+        vec3 d = vec3(0.263 , 0.416 , 0.557);
+        return a - b * cos(6.28318 * (c * t + d));
       }
 
       void main() {
-        vec2 uv = (gl_FragCoord.xy * -1.10 + resolution.xy) / resolution.y;      
+        vec2 uv = (gl_FragCoord.xy * -1.1 + resolution.xy) / resolution.xy;      
         vec2 uv0 = uv;
         vec3 finalColor = vec3(0.0);
-        uv = fract(uv * 3.25) - 0.5;     
+        uv = fract(uv * cos(3.25 / time)) - 0.5;     
         uv = sin(uv * 3.25) - (pointer);     
         float d = length(-uv) * exp(-length(uv0));
-        vec3 col = palette(length(uv0) + time);
+        vec3 col = palette(length(uv0) + time * 0.25);
         d = sin(d * 32.0 + time) / 8.0;
         d = abs(d);
         d = pow((0.25) / d, 1.);
